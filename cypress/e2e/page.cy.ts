63df39d51696b0e9026c3c70620a4db7edfb5891
Randy Asuncion <ranasunc@gmail.com>
2022-09-23 02:36:07 -0700

    add test for renaming pages

--- cypress/e2e/page.cy.ts
+++ cypress/e2e/page.cy.ts
@@ -1,6 +1,8 @@
 describe('Page', () => {
   it('shows initial page', () => {
     cy.visit('/');
+    cy.get('[data-cy=tab]').should('have.length', 1).contains('Page-1');
+    cy.get('[data-cy=tab-panel]').should('have.length', 1).should('be.visible');
   });
 
   it('can add a page', () => {
@@ -22,8 +24,19 @@ describe('Page', () => {
     cy.get('[data-cy=remove-page]').first().click();
     cy.get('[data-cy=tab]').first().click();
     cy.get('[data-cy=remove-page]').first().click();
-    cy.get('[data-cy=tab]').should('have.text', 'Page-3');
-    cy.get('[data-cy=tab]').should('have.length', 1);
-    cy.get('[data-cy=tab-panel]').should('have.length', 1);
+    cy.get('[data-cy=tab]').first().click();
+    cy.get('[data-cy=remove-page]').first().click();
+    cy.get('[data-cy=tab]').should('have.length', 0);
+    cy.get('[data-cy=tab-panel]').should('have.length', 0);
+  });
+
+  it('can rename a page', () => {
+    cy.visit('/', {
+      onBeforeLoad(win) {
+        cy.stub(win, 'prompt').returns('MyPage');
+      },
+    });
+    cy.get('[data-cy=tab]').first().dblclick();
+    cy.get('[data-cy=tab]').should('have.text', 'MyPage');
   });
 });
