d242ae0da890f7c6f5316e93c383bf458dedabb1
Randy Asuncion <ranasunc@gmail.com>
2022-09-24 03:03:33 -0700

    add data-cy attributes for cypress e2e tests

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -32,18 +32,20 @@ export const Menu: React.FC<MenuProps> = ({ text, children }) => {
   return (
     <div
       tabIndex={1}
+      data-cy={`${text.toLocaleLowerCase()}-menu`}
       onBlur={onBlur}
-      className={`px-2 py-1  cursor-default relative whitespace-nowrap ${
+      className={`px-2 py-1 outline-none cursor-default relative whitespace-nowrap ${
         hidden ? 'hover:bg-gray-100' : 'shadow-md'
       }`}
       onClick={onClick}
     >
       <div>{text}</div>
       <div
+        data-cy="menu-items"
         className={
           hidden
             ? 'hidden'
-            : 'absolute bg-white py-1 shadow-md min-w-[200px] -ml-2'
+            : 'absolute bg-white py-1 shadow-md min-w-[200px] -ml-2 outline-none'
         }
       >
         {children}
@@ -53,13 +55,20 @@ export const Menu: React.FC<MenuProps> = ({ text, children }) => {
 };
 
 interface MenuItemProps {
-  children: ReactNode;
+  children: string;
   onClick(): void;
 }
 
 export const MenuItem: React.FC<MenuItemProps> = ({ children, onClick }) => {
+  const alias =
+    children.toLocaleLowerCase().replaceAll(' ', '-') + '-menu-item';
+
   return (
-    <div className="hover:bg-gray-100 p-1 pl-5" onClick={onClick}>
+    <div
+      className="hover:bg-gray-100 p-1 pl-5"
+      onClick={onClick}
+      data-cy={alias}
+    >
       {children}
     </div>
   );
