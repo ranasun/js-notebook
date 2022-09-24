8677bf762400640968b27de69cb8a074e5344715
Randy Asuncion <ranasunc@gmail.com>
2022-09-24 05:46:53 -0700

    implement file menu actions

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,21 +6,64 @@ import './App.css';
 import { SunLight } from 'iconoir-react';
 import { MenuGroup, Menu, MenuItem } from '../components/Menu';
 import { useSelector, useDispatch } from 'react-redux';
-import { addBlankPage, removePage, renamePage } from './rootReducer';
+import {
+  setState,
+  resetState,
+  addBlankPage,
+  removePage,
+  renamePage,
+  renameNotebook,
+} from './rootReducer';
 import type { RootState } from '../app/store';
+import { downloadJSON } from '../common/utils';
+import { useRef } from 'react';
 
 const App = () => {
-  const { pages, order, active } = useSelector((state: RootState) => state);
+  const state = useSelector((state: RootState) => state);
+  const { pages, active, title } = state;
+  const file = useRef<any>();
+
   const dispatch = useDispatch();
 
-  const handleRenamePage = () => {
+  const onRenamePage = () => {
     const title = pages[active].title;
     const newTitle = prompt('Enter new title', title);
     if (newTitle) dispatch(renamePage({ pageId: active, title: newTitle }));
   };
 
+  const onDownload = () => {
+    downloadJSON(state.title + '.jsnb', state);
+  };
+
+  const onOpenNotebook = () => {
+    file.current.click();
+  };
+
+  const onFileUpload = (e: any) => {
+    const data = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (data: any) => {
+      const json = JSON.parse(data.target.result);
+      dispatch(setState(json));
+    };
+    reader.readAsText(data);
+    file.current.value = '';
+  };
+
+  const onSetTitle = () => {
+    const newTitle = prompt('Enter new title', title);
+    if (newTitle) dispatch(renameNotebook(newTitle));
+  };
+
   return (
     <div className="dark">
+      <input
+        ref={file}
+        type="file"
+        accept=".jsnb"
+        hidden
+        onChange={onFileUpload}
+      />
       <Navbar>
         <div className="container mx-auto">
           <div className="flex justify-between items-center">
@@ -28,8 +71,11 @@ const App = () => {
               <a className="font-light py-2" href="#">
                 JS Notebook
               </a>
-              <div className="ml-4 py-2 font-medium cursor-default hover:bg-gray-100">
-                MyNotebook
+              <div
+                className="ml-4 py-2 font-medium cursor-default hover:bg-gray-100"
+                onClick={onSetTitle}
+              >
+                {title}
               </div>
             </div>
             <div className="flex gap-2">
@@ -40,21 +86,26 @@ const App = () => {
         <hr />
         <MenuGroup>
           <Menu text="File">
+            <MenuItem onClick={() => dispatch(resetState())}>
+              New Notebook
+            </MenuItem>
+
+            <MenuItem onClick={onOpenNotebook}>Open Notebook</MenuItem>
+            <hr />
+            <MenuItem onClick={onSetTitle}>Save Notebook As</MenuItem>
+            <hr />
+            <MenuItem onClick={onDownload}>Download</MenuItem>
+          </Menu>
+          <Menu text="Page">
             <MenuItem onClick={() => dispatch(addBlankPage())}>
               New Page
             </MenuItem>
+            <MenuItem onClick={onRenamePage}>Rename Page</MenuItem>
             <hr />
-            <MenuItem onClick={() => {}}>New Notebook</MenuItem>
-            <MenuItem onClick={() => {}}>Save Notebook</MenuItem>
-            <hr />
-            <MenuItem onClick={() => {}}>Download</MenuItem>
-          </Menu>
-          <Menu text="Page">
             <MenuItem onClick={() => {}}>Run Page</MenuItem>
-            <MenuItem onClick={handleRenamePage}>Rename Page</MenuItem>
-            <hr />
+            {/* <hr />
             <MenuItem onClick={() => {}}>Reset Page</MenuItem>
-            <MenuItem onClick={() => {}}>Run and Reset Page</MenuItem>
+            <MenuItem onClick={() => {}}>Run and Reset Page</MenuItem> */}
             <hr />
             <MenuItem onClick={() => dispatch(removePage(active))}>
               Remove Page
--- src/common/types.ts
+++ src/common/types.ts
@@ -2,31 +2,32 @@ export type EntryType = 'code' | 'text';
 export type MoveDirection = 'up' | 'down';
 
 export interface Entry {
-    index: number;
-    entryId: string;
-    content: string;
-    type: EntryType;
+  index: number;
+  entryId: string;
+  content: string;
+  type: EntryType;
 }
 
 export interface Page {
-    pageId: string; // ex: 1p6lery1
-    title: string; // ex: Page-1
-    entries: Record<string, Entry>; // ex: { 'mm4bxyyt': Entry, 'cm6i2wa2': Entry, 'cxc34dey': Entry }
-    order: string[]; //ex: ['mm4bxyyt', 'cm6i2wa2', 'cxc34dey']
-    runCount: number;
-    inFocus: string;
-    codes: Record<string, string>;
+  pageId: string; // ex: 1p6lery1
+  title: string; // ex: Page-1
+  entries: Record<string, Entry>; // ex: { 'mm4bxyyt': Entry, 'cm6i2wa2': Entry, 'cxc34dey': Entry }
+  order: string[]; //ex: ['mm4bxyyt', 'cm6i2wa2', 'cxc34dey']
+  runCount: number;
+  inFocus: string;
+  codes: Record<string, string>;
 }
 
 export interface AppState {
-    pages: Record<string, Page>; // array of page ids | ex: ['1p6lery1']
-    active: string;
-    order: string[];
-    pageCount: number; // increases everytime a page is added. Does not decrement
+  pages: Record<string, Page>; // array of page ids | ex: ['1p6lery1']
+  active: string;
+  order: string[];
+  pageCount: number; // increases everytime a page is added. Does not decrement
+  title: string;
 }
 
 export interface Tab {
-    index: number;
-    name: string;
-    content: string;
+  index: number;
+  name: string;
+  content: string;
 }
