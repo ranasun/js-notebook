1a5d213f71187ea6c3f84efba6cd6ab169b04b4f
Randy Asuncion <ranasunc@gmail.com>
2022-09-24 07:00:33 -0700

    move persistor to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
+import { persistStore } from 'redux-persist';
 
 import {
   persistReducer,
@@ -32,3 +33,4 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store } from './app/store';
+import { store, persistor } from './app/store';
 import { Provider } from 'react-redux';
 import App from './app/App';
-
-let persistor = persistStore(store);
 
 const root = createRoot(document.getElementById('root') as Element);
 root.render(
