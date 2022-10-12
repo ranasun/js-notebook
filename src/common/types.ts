9a20746c41a3569797b88dd8065483a3b61d7458
Randy Asuncion <ranasunc@gmail.com>
2022-10-12 01:31:22 -0700

    Remove dev comments

--- src/common/types.ts
+++ src/common/types.ts
@@ -10,19 +10,19 @@ export interface Entry {
 }
 
 export interface Page {
-  pageId: string; // ex: 1p6lery1
-  title: string; // ex: Page-1
-  entries: Record<string, Entry>; // ex: { 'mm4bxyyt': Entry, 'cm6i2wa2': Entry, 'cxc34dey': Entry }
-  order: string[]; //ex: ['mm4bxyyt', 'cm6i2wa2', 'cxc34dey']
+  pageId: string;
+  title: string;
+  entries: Record<string, Entry>;
+  order: string[];
   runCount: number;
   inFocus: string;
 }
 
 export interface AppState {
-  pages: Record<string, Page>; // array of page ids | ex: ['1p6lery1']
+  pages: Record<string, Page>;
   active: string;
   order: string[];
-  pageCount: number; // increases everytime a page is added. Does not decrement
+  pageCount: number;
   title: string;
 }
 
