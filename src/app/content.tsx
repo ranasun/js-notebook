fd0cb2c74001d7886485a8b63d15142007fceb8b
Randy Asuncion <ranasunc@gmail.com>
2022-10-12 05:46:58 -0700

    Update initialState

--- src/app/content.tsx
+++ src/app/content.tsx
@@ -4,33 +4,68 @@ const initialState: AppState = {
   pages: {
     s7uuwdgo: {
       pageId: 's7uuwdgo',
-      title: 'Welcome',
+      title: 'Getting Started',
       entries: {
         i5s2sryt: {
           index: 1,
           entryId: 'i5s2sryt',
           content:
-            '# Welcome to JS Notebook\n\n> JS Notebook is a jupyter notebook clone for javascript\n\n### Features:\n1. Supports import statements\n2. Uses esbuild to bundle javascript codes\n3. Autosaves data using localStorage\n4. Supports Markdown',
+            '# Welcome to JS Notebook\n\nJS Notebook is a jupyter notebook clone for javascript. It is an interactive web application that combines live code with narrative text.',
           type: 'text',
         },
-        il9rda4h: {
+        tmrdzvvm: {
           index: 2,
-          entryId: 'il9rda4h',
-          content: '### Lets start with a simple React application',
+          entryId: 'tmrdzvvm',
+          content:
+            "### Let's start with a simple javascript code\nClick the code below to select it then press `Shift + Enter` to see the output.",
           type: 'text',
         },
-        f8of8dnu: {
+        bay3oy1k: {
           index: 3,
-          entryId: 'f8of8dnu',
-          content:
-            "import React, {useState} from 'react'\nimport ReactDOM from 'react-dom'\n\nconst App = () => {\n  const [count, setCount] = useState(0)\n  return (\n    <div>\n      <button onClick={() => setCount(count + 1)}>I've been clicked {count} times!</button>\n    </div>\n  )\n}\n\nconst root = ReactDOM.createRoot(document.querySelector('#root'));\nroot.render(<App/>)",
+          entryId: 'bay3oy1k',
+          content: "console.log('Hello from JS Notebook!')",
           type: 'code',
         },
-        rby4avg3: { index: 8, entryId: 'rby4avg3', content: '', type: 'code' },
+        zyhfb5jw: {
+          index: 4,
+          entryId: 'zyhfb5jw',
+          content: '### Each entry has its own #root element',
+          type: 'text',
+        },
+        '4uof6mms': {
+          index: 5,
+          entryId: '4uof6mms',
+          content:
+            'document.querySelector(\'#root\').innerHTML = "<h3>I am root</h3>"',
+          type: 'code',
+        },
+        w4txw3l7: {
+          index: 6,
+          entryId: 'w4txw3l7',
+          content: '### Import statements are supported',
+          type: 'text',
+        },
+        '3rapytnv': {
+          index: 7,
+          entryId: '3rapytnv',
+          content:
+            "import React, {useState} from 'react'\nimport {createRoot} from 'react-dom/client'\n  \nconst App = () => {\n  const [count, setCount] = useState(0)\n  return (\n    <div>\n      <button onClick={() => setCount(count + 1)}>I've been clicked {count} times!</button>\n    </div>\n  )\n}\n\nconst root = createRoot(document.querySelector('#root'));\nroot.render(<App/>)",
+          type: 'code',
+        },
+        q07ofzh6: { index: 8, entryId: 'q07ofzh6', content: '', type: 'code' },
       },
-      order: ['i5s2sryt', 'il9rda4h', 'f8of8dnu', 'rby4avg3'],
+      order: [
+        'i5s2sryt',
+        'tmrdzvvm',
+        'bay3oy1k',
+        'zyhfb5jw',
+        '4uof6mms',
+        'w4txw3l7',
+        '3rapytnv',
+        'q07ofzh6',
+      ],
       runCount: 8,
-      inFocus: 'rby4avg3',
+      inFocus: 'q07ofzh6',
     },
   },
   active: 's7uuwdgo',
